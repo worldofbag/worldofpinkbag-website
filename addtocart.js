bf248135c1af631daae6bdd61adcbd81df074d0a
worldofbag <155765378+worldofbag@users.noreply.github.com>
2024-01-06 02:18:22 +0800

    Add files via upload

---
+++ addtocart.js
@@ -0,0 +1,310 @@
+const product = [
+    {
+        id:0,
+        image: 'Untitled%20design%20(1).png',
+        title: 'JACKIE BAG SMALL PINK',
+        price: 11242.80,
+    },
+    {
+        id:1,
+        image: 'Untitled%20design%20(2).png',
+        title: 'BIRKIN JERLY',
+        price: 16753.00,
+    },
+    {
+        id:2,
+        image: 'Untitled%20design%20(3).png',
+        title: 'LAPSIE AMONIA',
+        price: 18901.00,
+    },
+    {
+        id:3,
+        image: 'Untitled%20design%20(4).png',
+        title: 'LAURENTIAL',
+        price: 13476.00,
+    },
+    {
+        id:4,
+        image: 'Untitled%20design%20(5).png',
+        title: 'CLASSIC PADERA',
+        price: 62450.00,
+    },
+    {
+        id:5,
+        image: 'Untitled%20design%20(6).png',
+        title: 'TEEN TRIOMPHE BAG',
+        price: 36500.00,
+    },
+    {
+        id:6,
+        image: 'Untitled%20design%20(7).png',
+        title: 'ROULIS HANDBAG',
+        price: 40253.91,
+    },
+    {
+        id:7,
+        image: 'Untitled%20design%20(8).png',
+        title: 'CUIR TRIOMPHE',
+        price: 4095.67,
+    },
+    {
+        id:8,
+        image: 'Untitled%20design%20(9).png',
+        title: 'FEARLESS',
+        price: 20098.00,
+    },
+    {
+        id:9,
+        image: 'Untitled%20design%20(10).png',
+        title: 'SWEET VANITY',
+        price: 18322.72,
+    },
+    {
+        id:10,
+        image: 'Untitled%20design%20(11).png',
+        title: 'JACKIE 1961 MEDIUM TOTE BAG',
+        price: 14715.00,
+    },
+    {
+        id:11,
+        image: 'Untitled%20design%20(12).png',
+        title: 'MINI LINDY ROSE SAKURA',
+        price: 40681.64,
+    },
+    {
+        id:12,
+        image: 'Untitled%20design%20(13).png',
+        title: 'KELLY SELLIER 20 ROSE SAKURA CHEVRE PALLADIUM HARDWARE',
+        price: 175668.75,
+    },
+    {
+        id:13,
+        image: 'Untitled%20design%20(14).png',
+        title: 'VANITY ZERA',
+        price: 8849.58,
+    },
+    {
+        id:14,
+        image: 'Untitled%20design%20(15).png',
+        title: 'CASSETTE',
+        price: 7342.00,
+    },
+    {
+        id:15,
+        image: 'Untitled%20design%20(16).png',
+        title: 'CASSETTEW BELT BAG',
+        price: 11321.84,
+    },
+    {
+        id:16,
+        image: 'Untitled%20design%20(17).png',
+        title: 'CANDIE JODIE',
+        price: 10752.84,
+    },
+    {
+        id:17,
+        image: 'Untitled%20design%20(18).png',
+        title: 'PINK CONJUNCTIVA',
+        price: 19876.00,
+    },
+    {
+        id:18,
+        image: 'Untitled%20design%20(19).png',
+        title: 'VENA CAVA',
+        price: 14350.00,
+    },
+    {
+        id:19,
+        image: 'Untitled%20design%20(20).png',
+        title: 'KELLY POCKET',
+        price: 37500.00,
+    },
+    {
+        id:20,
+        image: 'Untitled%20design%20(21).png',
+        title: 'Go-With-Man',
+        price: 254500.00,
+    },
+    {
+        id:21,
+        image: 'Untitled%20design%20(22).png',
+        title: 'FIRST MEDIUM',
+        price: 9070.00,
+    },
+    {
+        id:22,
+        image: 'Untitled%20design%20(23).png',
+        title: 'VANITY GERENA',
+        price: 17999.00,
+    },
+    {
+        id:23,
+        image: 'Untitled%20design%20(24).png',
+        title: 'SWEET BOMB',
+        price: 19300.00,
+    },
+    {
+        id:24,
+        image: 'Untitled%20design%20(25).png',
+        title: 'CRY FOR ME',
+        price: 165070.0,
+    },
+    {
+        id:25,
+        image: 'Untitled%20design%20(26).png',
+        title: 'BUBBLE MIST',
+        price: 143500.00,
+    },
+    {
+        id:26,
+        image: 'Untitled%20design%20(27).png',
+        title: 'MINI CLAUDE',
+        price: 14500.00,
+    },
+    {
+        id:27,
+        image: 'Untitled%20design%20(29).png',
+        title: 'Go-With-Man',
+        price: 18970.00,
+    },
+    {
+        id:28,
+        image: 'Untitled%20design%20(30).png',
+        title: 'GIVENCI LARONDA',
+        price: 56421.00,
+    },
+    {
+        id:29,
+        image: 'Untitled%20design%20(31).png',
+        title: 'TULIPS SELORA',
+        price: 10980.00,
+    },
+    {
+        id:30,
+        image: 'Untitled%20design%20(32).png',
+        title: 'BISTRO BASKET BAG PINK',
+        price: 6407.00,
+    },
+    {
+        id:31,
+        image: 'Untitled%20design%20(33).png',
+        title: 'PINKY VENNA',
+        price: 176800.00,
+    },
+    {
+        id:32,
+        image: 'Untitled%20design%20(35).png',
+        title: 'YONITY VEHENA',
+        price: 16707.0,
+    },
+    {
+        id:33,
+        image: 'Untitled%20design%20(36).png',
+        title: 'VIST SINARA',
+        price: 23500.00,
+    },
+    {
+        id:34,
+        image: 'Untitled%20design%20(37).png',
+        title: 'MINI JODIE MINI BAG BLISS WASHED GOLD',
+        price: 12416.00,
+    },
+    {
+        id:35,
+        image: 'Untitled%20design%20(38).png',
+        title: 'KATE BLISS',
+        price: 75900.00,
+    },
+    {
+        id:36,
+        image: 'Untitled%20design%20(39).png',
+        title: 'JOE AND KATE',
+        price: 35400.00,
+    },
+    {
+        id:37,
+        image: 'Untitled%20design%20(40).png',
+        title: 'BRIKIN SORONA',
+        price: 142300.00,
+    },
+    {
+        id:38,
+        image: 'Untitled%20design%20(41).png',
+        title: 'GoVENCY SAKURA',
+        price: 17900.00,
+    },
+    {
+        id:39,
+        image: 'Untitled%20design%20(42).png',
+        title: 'BIRKIN 25 ROSE SAKURA SWIFT ',
+        price: 127500.00,
+    },
+    {
+        id:40,
+        image: 'Untitled%20design%20(43).png',
+        title: 'MINI VANITY CASE',
+        price: 8002.89,
+    },
+    {
+        id:41,
+        image: 'Untitled%20design%20(44).png',
+        title: 'KATE WISS',
+        price: 678300.00,
+    },
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image ,title ,price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(a){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "RM "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "RM "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+(j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+}
